9b427f34280d19ff3881ca6ecabcf72ab7ee39a0
Zeeeyah <147207991+Zeeeyah@users.noreply.github.com>
2024-01-19 21:09:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('New healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-boiled Free-Range Egg')
+streamlit.text ('🥑🍞Avocado Toast')
+
+streamlit.header ('🍌🥭Build your own smoothie🥝🍇')
